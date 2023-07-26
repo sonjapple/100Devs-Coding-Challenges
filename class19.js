73a26520d83ef909637e1ecf13880c00300426ab
Sonja Joubert <sonjacb@gmail.com>
2023-07-26 10:12:16 +0200

    homework added

--- class19.js
+++ class19.js
@@ -80,7 +80,13 @@ isEven(-2) //??
 //Bean Counting
 
 function countBs(str){
-  for(let i = 0; i < str.length; i++){
-    
+  let count = 0
+  for(let i = 0; i <= str.length-1; i++){
+    if(str[i] === "B"){
+      count += 1
+    }
   }
+  return count
 }
+console.log(countBs("ButterBall"))
+
